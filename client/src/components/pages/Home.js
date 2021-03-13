4939b591c978af6353cbee576ae14bb47b3556ae
Erickson Powell <ericksonpowell@gmail.com>
2021-03-13 12:50:15 -0800

    installed material-ui and started basic front routing

---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { Fragment } from "react";
+import { Button } from "@material-ui/core";
+
+export default () => {
+  return(
+    <Fragment>
+      <Button variant="contained" color="primary">
+      Hello World
+      </Button>
+
+      
+
+    </Fragment>
+  );
+}
