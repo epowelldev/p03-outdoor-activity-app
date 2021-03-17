0b84cfbf2d8a15b48915453631c3f01d4d6112a8
Tristan Bond <tristanebond@gmail.com>
2021-03-16 21:42:55 -0700

    login page state and logic

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 import {Button, Input, Box} from '@material-ui/core';
-
+import API from "../utils/API"
+import { useEffect, useState } from 'react';
+import { Redirect } from "react-router-dom";
 
 const useStyles=makeStyles({
   formStyles:{
@@ -23,13 +25,50 @@ const useStyles=makeStyles({
 
 function Login(){
   const classes = useStyles();
+
+
+  const [loginState, setLoginState] = useState({ username: '', password: '' });
+  const { username, password } = loginState;
+  const [logOutState, setLogOutState] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+      API.logout();
+
+  }, [logOutState]);
+
+
+
+  function handleSubmit(e) {
+      e.preventDefault()
+      API.login(username, password).then((res) => {
+          console.log(res.data)
+          setLoginState({ username: '', email: '', password: '' })
+       setLoggedIn(true)
+      })
+      console.log(loginState)
+  }
+
+
+  function handleChange(e) {
+      e.preventDefault();
+      setLoginState({ ...loginState, [e.target.name]: e.target.value })
+      console.log(loginState)
+  }
+
+
+
+
+if(loggedIn){
+  return <Redirect to={{ pathname: "/Events" }} />;
+}else{
 return(
-<form > 
+<form onSubmit={handleSubmit}> 
  
   <Box className={classes.formStyles}>
 
-    <Input placeholder="UserName" className={classes.inputStyles} inputProps={{ 'aria-label': 'description' }} />
-    <Input placeholder="Password" className={classes.inputStyles}inputProps={{ 'aria-label': 'description' }} />
+    <Input placeholder="UserName" className={classes.inputStyles} name="username" value={username} onChange={handleChange} inputProps={{ 'aria-label': 'description' }} />
+    <Input placeholder="Password" className={classes.inputStyles} name="password" value={password} onChange={handleChange} inputProps={{ 'aria-label': 'description' }} />
     <Button variant="contained" className={classes.inputStyles} color="primary">
         Login
     </Button>
@@ -38,7 +77,9 @@ return(
   </Box>
 
 </form>
+
 );
 }
-// className={classes.root} noValidate autoComplete="off"
+}
+
 export default Login;
