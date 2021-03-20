470ff20dbf58e41218e193909a86ba7ce5c10d40
Erickson Powell <ericksonpowell@gmail.com>
2021-03-20 10:52:12 -0700

    started context code

---
+++ client/src/context/app/appReducer.js
@@ -0,0 +1,12 @@
+import {} from "../types";
+
+export default (state, action) => {
+  switch(action.type) {
+    case :
+      return ;
+    case :
+      return ;
+    default:
+      return state;
+  }
+}
